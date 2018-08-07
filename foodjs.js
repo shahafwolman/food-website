e68b7c6e7a55ef2ba7ac7644d044cfd3ddad9db4
unknown <jgutke@outlook.com>
2018-08-07 14:14:22 -0700

    m

--- foodjs.js
+++ foodjs.js
@@ -1,7 +1,9 @@
 function makeGray(a){
-    a.style.backgroundColor= "gray";
+    a.style.backgroundColor= "#c4bfaf";
+    a.style.color = "black";
 }
 
 function makeBlack(b){
-  b.style.backgroundColor = "black"
+  b.style.backgroundColor = "black";
+  b.style.color = "white";
 }
