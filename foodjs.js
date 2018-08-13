010e4e42d5a6fc8d22d17313a15a849a93802ba8
unknown <shahaf.wolman@gmail.com>
2018-08-13 10:24:00 -0700

    fixes webpage

--- foodjs.js
+++ foodjs.js
@@ -114,11 +114,11 @@ map.entities.push(pushpin);
 }
 
 
-(function() {
+
 	function Slideshow( element ) {
 		this.el = document.querySelector( element );
 		this.init();
-	}
+
 	Slideshow.prototype = {
 		init: function() {
 			this.wrapper = this.el.querySelector( ".slider-wrapper" );
@@ -163,7 +163,7 @@ map.entities.push(pushpin);
 		}
   }
 }
-)};
+};
 	document.addEventListener( "DOMContentLoaded", function() {
 		var slider = new Slideshow( "#main-slider" );
 	});
