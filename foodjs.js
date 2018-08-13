290d4948bc85df7c3a40d2922fd56543b0cfd541
JuliaNichols <41652973+JuliaNichols@users.noreply.github.com>
2018-08-13 15:15:20 -0700

    h

--- foodjs.js
+++ foodjs.js
@@ -9,16 +9,6 @@ function makeBlack(b){
   b.style.color = "white";
 }
 
-
-
-/*var view;
-var map, infobox;
-var ourLoc;
-var Lats = [30.5423159,30.5219341,30.5224853,	30.6035039];
-var Long = [-87.90039519999999, -87.9031948,-87.90536859999997,-87.90292649999998];*/
-
-
-
           /*map.setView;{(
            center): new Microsoft.Maps.Location(42.877742, -97.380979),
            zoom = 4
@@ -26,18 +16,25 @@ var Long = [-87.90039519999999, -87.9031948,-87.90536859999997,-87.9029264999999
 
 
 var map, infobox;
-var lat = [30.5423159,30.5219341,30.5224853,	30.6035039];
-var long = [-87.90039519999999, -87.9031948,-87.90536859999997,-87.90292649999998];
-var titles = ["Sunset Point", "Panini Petes", "Gumbo Shack", "Manci Antique Club"]
-var descriptions = ["(251) 990-7766 831 N. Section Street Fairhope, Alabama", "hi"]
+var lat = ["47.750717", "47.744189", "47.01971", "47.745978 ", "47.64225", "47.278928", "48.517722", "48.51429", "48.15278",
+ "48.194622", "48.194315" , "48.187093", "48.01305", "48.009382", "47.306701", "47.307505", "47.624827", "45.780731"];
+var long = ["-122.562177", "-122.575892", "-123.798449", "-122.577684", "-117.58811", "-122.24966","-122.612551", "-122.612621", "-122.1804",
+ "-122.12607", "-122.12607", "-122.13515", "-122.53807", "-122.52883", "-122.23071", "-122.22134", "-122.516546", "-122.531"];
+var titles = ["The Pink Door", "Pick Place Chowder", "Ocean Palace Resturant ", "Zig Zag Cafe", "Wolffys Hamburgers", "Tacos El Tajin",
+"Adrift", "Dads Dinner", "Moose Creek BBQ", "Bistro San Martin", "The Stilly Diner", "Hubbs Pizza & Pasta", "Gordon's On Blueberry Hill",
+"Rocket Taco", "Sun Break Cafe", "Gor Gai Thai Street Food", "Streamliner Diner", "Barrel Mountain Brewing"]
+var descriptions = ["20820 Miller Bay Rd NE, Poulsbo, WA 98370, USA", "20235 Robin Ln NE, Suquamish, WA 98392, USA", "http://oceanpalaceaberdeen.com/", "20329 Robin Ln NE, Suquamish, WA 98392, USA", "12807 W 14th Ave, Airway Heights, WA 99001", "400 1st Ave N, Algona, WA 98001",
+"510 Commercial Ave, Anacortes, WA 98221", " 906 Commercial Ave, Anacortes, WA 98221", "3617 - 172nd St NE Ste 5, Arlington, WA 98223", "231 N Olympic Ave, Arlington, WA 98223", " 223 N Olympic Ave, Arlington, WA 98223", "21102 67th Ave NE, Arlington, WA 98223", "5438 Woodard Ave, Freeland, WA 98249",
+"1594 Main St, Freeland, WA 98249 ", "22 A St SW, Auburn, WA 98001", "614 E Main St, Auburn, WA 98002 ", "397 Winslow Way E, Bainbridge Island, WA 98110", "607 E Main St, Battle Ground, WA 98604"]
+var website = ["",]
 function GetMap() {
     map = new Microsoft.Maps.Map('#myMap', {
         credentials: 'AoYDlUxB1n_Wkk8nKDVnVCFD_SzWWkxX5NoAiYNYhILk2ZXTrYLfe7LMgYash4y1'
     });
 
     map.setView({
-     center: new Microsoft.Maps.Location(42.877742, -97.380979),
-     zoom: 4});
+     center: new Microsoft.Maps.Location(47.751076, -120.740135),
+     zoom: 7});
 
     //Create an infobox at the center of the map but don't show it.
     infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
@@ -56,7 +53,8 @@ function GetMap() {
 
         //Store some metadata with the pushpin.
         pin.metadata = {
-            title: (titles[i] + descriptions[i])
+            title: titles[i],
+            description: descriptions[i] + '<a href = website[i]>link</a>'
           };
 
 
