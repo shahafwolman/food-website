eb88e1aff22c9a71a236d6f5693896e34f5d9ec5
JuliaNichols <41652973+JuliaNichols@users.noreply.github.com>
2018-08-15 13:58:03 -0700

    h

--- foodjs.js
+++ foodjs.js
@@ -150,7 +150,6 @@ $(function(){
          .end().appendTo('#main-slider');},
       3000);
 });
-<<<<<<< HEAD
 
 
 
@@ -201,78 +200,59 @@ $(function(){
 			}, false);
 		}
   }
-};
+}
 
 	document.addEventListener( "DOMContentLoaded", function() {
 		var slider = new Slideshow( "#main-slider" );
 	});
 
-=======
 
 
->>>>>>> 1dc470fe2777ef4b919f31e9be7fa672faa8c477
-  // Extra things to add to bottom
-  filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column6");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-<<<<<<< HEAD
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-=======
-// var btnContainer = document.getElementById("myBtnContainer");
-// var btns = btnContainer.getElementsByClassName("btn");
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function(){
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
-
->>>>>>> 1dc470fe2777ef4b919f31e9be7fa672faa8c477
-  function foodTitle(ft){
-      ft.style.backgroundColor= "#c4bfaf";
-      ft.style.color = "black";
-  }
+                              // Extra things to add to bottom
+  // filterSelection("all") // Execute the function and show all columns
+                    // function filterSelection(c) {
+                    //   var x, i;
+                    //   x = document.getElementsByClassName("column");
+                    //   if (c == "all") c = "";
+                    //   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+                    //   for (i = 0; i < x.length; i++) {
+                    //     w3RemoveClass(x[i], "show");
+                    //     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+                    //   }
+                    // }
+                    //
+                    // // Show filtered elements
+                    // function w3AddClass(element, name) {
+                    //   var i, arr1, arr2;
+                    //   arr1 = element.className.split(" ");
+                    //   arr2 = name.split(" ");
+                    //   for (i = 0; i < arr2.length; i++) {
+                    //     if (arr1.indexOf(arr2[i]) == -1) {
+                    //       element.className += " " + arr2[i];
+                    //     }
+                    //   }
+                    // }
+                    //
+                    // // Hide elements that are not selected
+                    // function w3RemoveClass(element, name) {
+                    //   var i, arr1, arr2;
+                    //   arr1 = element.className.split(" ");
+                    //   arr2 = name.split(" ");
+                    //   for (i = 0; i < arr2.length; i++) {
+                    //     while (arr1.indexOf(arr2[i]) > -1) {
+                    //       arr1.splice(arr1.indexOf(arr2[i]), 1);
+                    //     }
+                    //   }
+                    //   element.className = arr1.join(" ");
+                    // }
+                    //
+                    // // Add active class to the current button (highlight it)
+                    // var btnContainer = document.getElementById("myBtnContainer");
+                    // var btns = btnContainer.getElementsByClassName("btn");
+                    // for (var i = 0; i < btns.length; i++) {
+                    //   btns[i].addEventListener("click", function(){
+                    //     var current = document.getElementsByClassName("active");
+                    //     current[0].className = current[0].className.replace(" active", "");
+                    //     this.className += " active";
+                    //   });
+                    // }
